de75c1aedad8a4938295b45718ff3402f9af399f
Jarron Futter <jarronfutter@gmail.com>
2023-05-03 14:45:13 +0200

    Add initial commit files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "@picocss/pico";
+
+const App = () => {
+    return (
+        <div className="container">
+            <h2>Exciting New Cocktail application!</h2>
+            <p>index.tsx is the entry point</p>
+            <p>App.tsx sets up the auth and routing</p>
+        </div>
+    );
+};
+
+export default App;
