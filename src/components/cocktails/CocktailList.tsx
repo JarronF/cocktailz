5600f2b7bc60bd049ca10e03f109359e584702a8
Jarron Futter <jarronfutter@gmail.com>
2023-05-23 13:29:46 +0200

    add loading and extract randomise util func

--- src/components/cocktails/CocktailList.tsx
+++ src/components/cocktails/CocktailList.tsx
@@ -1,7 +1,11 @@
 import axios from "axios";
 import { ICocktail } from "@/models/ICocktail";
 import CocktailItem from "components/cocktails/CocktailItem";
-import { getRandomNumber, getFeaturedDrinks } from "../../utils/cocktail-util";
+import LoadingIndicator from "components/ui/LoadingIndicator";
+import {
+    getRandomDrinkIndices,
+    getFeaturedDrinks,
+} from "../../utils/cocktail-util";
 import { useState, useEffect } from "react";
 
 const urlQuery = `${import.meta.env.VITE_API_BASEURL}filter.php?c=Cocktail`;
@@ -11,56 +15,49 @@ const CocktailList: React.FC = () => {
     const [featuredDrinks, setFeaturedDrinks] = useState<ICocktail[] | null>(
         null
     );
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
     useEffect(() => {
-        const drinkIndices: number[] = [];
-        const getDrinks = async () => {
-            const response = await axios.get(urlQuery);
-            const drinkList: ICocktail[] = response.data.drinks;
-            for (let i = 0; i < displayQty; ) {
-                const random = getRandomNumber(1, drinkList.length - 1);
-                if (drinkIndices.includes(random)) {
-                    //ensure only unique drink ids are added
-                    continue;
-                }
-                drinkIndices.push(random);
-                i++;
-            }
+        setIsLoading(true);
 
-            setFeaturedDrinks(getFeaturedDrinks(drinkList, drinkIndices));
+        const getDrinks = async () => {
+            try {
+                const response = await axios.get(urlQuery);
+                const drinkList: ICocktail[] = response.data.drinks;
+                const drinkIndices = getRandomDrinkIndices(
+                    drinkList,
+                    displayQty
+                );
+                setFeaturedDrinks(getFeaturedDrinks(drinkList, drinkIndices));
+            } catch (err) {
+                //TODO: need to show this in some kind of user feedback
+                console.log(err);
+            } finally {
+                setIsLoading(false);
+            }
         };
 
-        getDrinks().catch(console.error);
+        getDrinks();
     }, []);
 
-    const displayDrinkList = () => {
-        if (featuredDrinks) {
-            return (
-                <article>
-                    <header>
-                        <h4>Featured Cocktails</h4>
-                    </header>
-                    <div className="grid">
-                        {featuredDrinks.map((drink) => (
-                            <CocktailItem drink={drink} key={drink.idDrink} />
-                        ))}
-                    </div>
-                </article>
-            );
-        } else {
-            return (
-                <article>
-                    <header>
-                        <p aria-busy="true">
-                            Loading featured Cocktails, please wait...
-                        </p>
-                    </header>
-                </article>
-            );
-        }
-    };
+    if (isLoading) return <LoadingIndicator />;
 
-    return displayDrinkList();
+    return (
+        <article>
+            <header>
+                <h4>Featured Cocktails</h4>
+            </header>
+            <div className="grid">
+                {!featuredDrinks && (
+                    <p>There was a problem loading featured drinks</p>
+                )}
+                {featuredDrinks &&
+                    featuredDrinks.map((drink) => (
+                        <CocktailItem drink={drink} key={drink.idDrink} />
+                    ))}
+            </div>
+        </article>
+    );
 };
 
 export default CocktailList;
